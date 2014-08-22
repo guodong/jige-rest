69a9a6cd89207fdff58729c21cac4c455fbbce1e
Ivan Zhao <752251080@qq.com>
2014-08-23 02:38:17 +0800

    add sell?type=uid&q=xxx

--- Api/Sell.php
+++ Api/Sell.php
@@ -39,27 +39,34 @@ class Sell extends Api
     public function all()
     {
         $data = Request::getData();
-        $c = new Collection('sellinfo');
-        $result = @split('#',$data['q']);
-        $params = array();
-        foreach($result as $r ){
-        	if($r != ""){
-        		$params[] = $r;
-        	}
+        if($data['type']=='uid'){
+        	$c = new Collection('sellinfo');
+        	$data = Request::getInstance()->getData();
+        	$d = $c->findAll('seller_id=?', array($data['q']));
+        	Response::sendSuccess($d);
+        }else{
+	        $c = new Collection('sellinfo');
+	        $result = @split('#',$data['q']);
+	        $params = array();
+	        foreach($result as $r ){
+	        	if($r != ""){
+	        		$params[] = $r;
+	        	}
+	        }
+	        $sql = "SELECT si.id,si.book_id,si.seller_id,si.`status`,si.price,si.campus_id,si.contact,si.college_id,si.`describe`,si.pics,si.time".
+	          " FROM bookinfo AS bi ,sellinfo AS si WHERE bi.id = si.book_id AND (";
+	        $flag = 0;
+	        for($i = 0;$i < count($params);$i++){
+	        	if($flag == 0){
+	        		$sql = $sql . 'bi.`search` LIKE \'%'.$params[$i].'%\'';
+	        		$flag = 1;
+	        	}else{
+	        		$sql = $sql . ' OR bi.`search` LIKE \'%'.$params[$i].'%\'';
+	        	}
+	        }
+	        $sql = $sql.')';
+	        $ret = Db::sql($sql);
+	        Response::sendSuccess($ret);
         }
-        $sql = "SELECT si.id,si.book_id,si.seller_id,si.`status`,si.price,si.campus_id,si.contact,si.college_id,si.`describe`,si.pics,si.time".
-          " FROM bookinfo AS bi ,sellinfo AS si WHERE bi.id = si.book_id AND (";
-        $flag = 0;
-        for($i = 0;$i < count($params);$i++){
-        	if($flag == 0){
-        		$sql = $sql . 'bi.`search` LIKE \'%'.$params[$i].'%\'';
-        		$flag = 1;
-        	}else{
-        		$sql = $sql . ' OR bi.`search` LIKE \'%'.$params[$i].'%\'';
-        	}
-        }
-        $sql = $sql.')';
-        $ret = Db::sql($sql);
-        Response::sendSuccess($ret);
     }
 }
