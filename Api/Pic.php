c593d9a5cc47a2118da8241be60d562ed7e5f3ec
guodong <gd@tongjo.com>
2014-08-23 08:00:33 +0800

    add pic

---
+++ Api/Pic.php
@@ -0,0 +1,23 @@
+<?php
+namespace Api;
+use Pest\Api;
+use Pest\Request;
+use Pest\Db\Collection;
+use Pest\Response;
+
+class Pic extends Api
+{
+
+    public $post = array(
+            
+    );
+
+    public function post ()
+    {
+        $uploads_dir = '/var/web/vhost/jige-rest/public/static/uploads';
+        $tmp_name = $_FILES["pic"]["tmp_name"];
+        $name = (string) new \MongoId(null).'.jpg';
+        move_uploaded_file($tmp_name, "$uploads_dir/$name");
+        echo '<script>document.domain="mallschool.me";parent.loadfile("'.$name.'")</script>';
+    }
+}
