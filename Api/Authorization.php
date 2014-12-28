a9ef9563f7319cce57ec3d77dfa274df2182fa72
Ivan Zhao <752251080@qq.com>
2014-12-28 12:49:54 +0800

    添加用户授权api

---
+++ Api/Authorization.php
@@ -0,0 +1,41 @@
+<?php
+namespace Api;
+use Pest\Api;
+use Pest\Db\Collection;
+use Pest\Request;
+use Pest\Response;
+class Authorization extends Api
+{
+    public $get = array(
+    		'code' => '/^\S{4,}$/',
+    );
+    
+    public function get ()
+    {
+    	$data = Request::getInstance()->getData();
+    	$c = new Collection('authorization');
+    	$ret = $c->findOne("code = ? " , array($data["code"]));
+    	if($ret){
+    		Response::sendSuccess($ret);
+    	}else{
+    		Response::sendFailure(1000);
+    	}
+    }
+
+    public $post = array(
+    		'openid' => '/^\S{28}$/',
+    );
+    
+    public function post ()
+    {
+    	$data = Request::getInstance()->getData();
+    	$c = new Collection('authorization');
+    	if(isset($data['time'])){
+    		unset($data['time']);
+    	}
+    	$data['code'] = substr(strtoupper(md5(uniqid(mt_rand(), true))),1,6);
+    	$data['time'] = time();
+    	$ret = $c->save($data);
+    	Response::sendSuccess($ret);
+    }
+}
